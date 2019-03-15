f0b8d145c98c90464f6a1bce1a09e310cdf55c12
Octo-Mar <miriam.arlt@st.oth-regensburg.de>
2019-03-15 18:09:25 +0100

    First mini steps

---
+++ Angular_Project/my-first-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server', //'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  styles: [`
+  h3 {
+    color: dodgerblue:
+  }
+  `]
+})
+export class AppComponent {
+}
---
+++ Angular_Project/my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { AppComponent } from './../app.component';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  template: '<app-server></app-server><app-server></app-server>',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
